006a7bc1650f1c39b020a35366abcb37032b29a6
Kyle J. Bilton <kjbilton@gmail.com>
2017-12-12 11:40:34 -0800

    Added docstring to coord parser

--- scripts/oaktext.py
+++ scripts/oaktext.py
@@ -2,7 +2,24 @@ import re
 import numpy as np
 
 def get_coords(addr):
-    coords = re.findall('[\d.]+, [\-\d.]+', addr)
+    """Extract the latitude and longitude from a string containing these values
+
+    Parameters
+    ----------
+    addr : str
+        String containing latitude and longitude, separated by a comma
+
+    Returns
+    -------
+    coords : tuple
+        Tuple (latitude, longitude)
+
+    Example
+    -------
+    >>> get_coords('123 Fake St., Berkeley, CA (-123.456, 78.9)')
+    >>> (-123.456, 78.9)
+    """
+    coords = re.findall('[\-\d.]+, [\-\d.]+', addr)
 
     # Return a missing value if there aren't any coordinates
     if not coords:
--- test/test_oaktext.py
+++ test/test_oaktext.py
@@ -13,3 +13,8 @@ def test_get_coords():
 
     # Ensure we get the right coordinates out
     assert coords == (100, 40)
+
+    # Check negative numbers
+    addr = '12322 Fake 222street, (-12312312100, -421320)'
+    coords = get_coords(addr)
+    assert coords == (-12312312100, -421320)
